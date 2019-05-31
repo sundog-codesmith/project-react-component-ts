06da813dbcc9e26388cbe0b6bde6e67612644be3
lwyj123 <liang.pearce@gmail.com>
2019-05-31 11:01:18 +0800

    feat: init

---
+++ stories/index.stories.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+import { storiesOf } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
+import { linkTo } from '@storybook/addon-links';
+
+// import { Button, Welcome } from '@storybook/react/demo';
+
+import { TestComponent } from '../src/components/TestComponent/TestComponent'
+
+
+const stories = storiesOf('Components', module);
+
+stories.add(
+  'TestComponent',
+  () => <TestComponent text={"wtf"} />,
+  {}
+);
+
+// storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
+
+// storiesOf('TestComponent', module)
+//   .add('with text', () => <Button onClick={action('clicked')}>Hello Button</Button>)
+//   .add('with some emoji', () => (
+//     <Button onClick={action('clicked')}>
+//       <span role="img" aria-label="so cool">
+//         😀 😎 👍 💯
+//       </span>
+//     </Button>
+//   ));
